c85ff1486cf26c4cca4a6f5413bfd85b7b5ddf89
Erdem Kaya <kerdem@live.com>
2024-11-12 13:20:34 +0100

    BlogPosting Schema

--- src/components/Blog/BlogDetail.jsx
+++ src/components/Blog/BlogDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import blogs from '../../../data/blogs.json';
+import { Helmet } from 'react-helmet-async';
 
 function BlogDetail() {
   const { slug } = useParams();
@@ -18,7 +19,36 @@ function BlogDetail() {
           <p className='blog-detail-content'>{blog.content}</p>
         </div>
       </div>
-
+      {/* BlogPosting Schema */}
+      <Helmet>
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "BlogPosting",
+            "mainEntityOfPage": {
+              "@type": "WebPage",
+              "@id": `https://www.de-kaya.com/blog/${blog.slug}`
+            },
+            "headline": blog.title,
+            "description": blog.metaDescription,
+            "datePublished": "2023-11-01T08:00:00+08:00", // Yayınlanma tarihini ISO formatında ekleyin
+            "dateModified": "2023-11-01T08:00:00+08:00", // Güncellenme tarihi
+            "author": {
+              "@type": "Person",
+              "name": "Ekrem Kaya" // Yazar adı
+            },
+            "publisher": {
+              "@type": "Organization",
+              "name": "De-Kaya",
+              "logo": {
+                "@type": "ImageObject",
+                "url": "https://www.de-kaya.com/images/dekaya-logo.png" // Logonuzun URL'si
+              }
+            },
+            "image": "https://www.de-kaya.com/images/seyyarsogutma.png" // Blog yazısına ait ana görsel URL'si
+          })}
+        </script>
+      </Helmet>
     </div>
   );
 }
